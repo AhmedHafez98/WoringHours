8cf40a051247126dcc60e6f6a57c04e1e7e35a5e
AhmedHafez98 <ah.hafez1098@gmail.com>
2020-04-10 02:44:51 +0200

    Design Form1.1

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ class Main(QMainWindow,Designs.GUI.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.lineEdit_2.returnPressed=self.keyPressEvent
+        self.lineEdit_2.returnPressed.connect(self.fu)
         self.btns=list()
         self.vbox=QVBoxLayout()
         self.tabWidget.tabBar().hide()
         self.bdel.clicked.connect(self.fun)
         self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    def keyPressEvent(self, e):
-        nbtb=QCommandLinkButton()
-        nbtb.setText(self.lineEdit_2.text())
+    def fu(self):
+        try:
+           nbtb=QCommandLinkButton(self.lineEdit_2.text())
+        except Exception as ex:print(ex)
         self.vbox.addWidget(nbtb)
         self.verticalLayout_4.addLayout(self.vbox)
-        nbtb.setMaximumSize(152,50)
+        nbtb.setMaximumHeight(50)
     def fun(self):
         self.vbox.itemAt(0).widget().deleteLater()
 
